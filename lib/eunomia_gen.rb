b1f1b10cd1c330dfb0e5053076ca4bd742113fff
G Palmer <palmergs@gmail.com>
2024-08-25 15:56:26 -0400

    initial checkin with rust extension

---
+++ lib/eunomia_gen.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "eunomia_gen/version"
+require_relative "eunomia_gen/eunomia_gen"
+
+module EunomiaGen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/eunomia_gen/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EunomiaGen
+  VERSION = "0.1.0"
+end
